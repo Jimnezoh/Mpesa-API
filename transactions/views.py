f6ebc1eb2769ac54e639ee978a24751e2bf574ff
Jimnezoh <jimmaina100@gmail.com>
2025-06-03 14:13:34 +0300

    generated access tokens

--- transactions/views.py
+++ transactions/views.py
@@ -1,7 +1,41 @@
+import os
 from django.shortcuts import render
 from .forms import PaymentForm
+from dotenv import load_dotenv
+import base64
 
 # Create your views here.
+load_dotenv()
+
+#retrieving variables from the env file
+CONSUMER_KEY = os.getenv('CONSUMER_KEY')
+CONSUMER_SECRET = os.getenv('CONSUMER_SECRET')
+MPESA_PASSKEY = os.getenv('MPESA_PASSKEY')
+MPESA_SHORTCODE = os.getenv('MPESA_SHORTCODE')
+MPESA_BASE_URL = os.getenv('MPESA_BASE_URL')
+CALLBACK_URL = os.getenv('CALLBACK_URL')
+
+#generate M-pesa access token
+def generate_access_token():
+    try:
+        credentials = f"{CONSUMER_KEY}:{CONSUMER_SECRET}"
+        encoded_credentials = base64.b64encode(credentials.encode()).decode()
+
+        headers = {
+            "Authorization": f"Basic {encoded_credentials}",
+            "Content-Type": "application/json"
+        }
+        response = requests.get(
+            f"{MPESA_BASE_URL}/oauth/v1/generate?grant_type=client_credentials",
+            headers=headers
+        ).json()
+
+        if "access_token" in response:
+            return response["access_token"]
+        else:
+            raise Exception("Failed to generate access token")
+    except requests.RequestException as e:
+        print(f"Failed to connect to M-Pesa: {str(e)}")
 
 def index(request):
     form = PaymentForm()
